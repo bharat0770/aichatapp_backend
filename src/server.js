6ab1d9cd91ab7e6b3c20daa58de1cc8a04b1d838
Bharat Suthar <154978824+bharat0770@users.noreply.github.com>
2025-12-31 12:17:01 +0530

    added frontend url to handle cors

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', process.env.FRONTEND_URL],
 }));
 
 app.use("/api/v1/user", userRoutes);
